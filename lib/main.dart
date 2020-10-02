cafa59fd7e7c96943231123e68938a1c97c0f3b8
Aditya Sharma <aditya25dec2000@gmail.com>
2020-10-02 11:45:18 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Expenses Manager",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expenses Manager"),
+      ),
+      body: Center(
+        child: Text("Hey"),
+      ),
+    );
+  }
+}
